4b3e2fb15528db194e7894079750554a04dd15bb
Coffe <temporavel2@gmail.com>
2026-07-01 10:44:22 -0300

    Update and rename api/books.js to redacoes.js

---
+++ redacoes.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+    const token = process.env.X_API_KEY;
+    try {
+        const response = await fetch('https://edusp-api.ip.tv/tms/task/todo?is_essay=true&answer_statuses=draft&answer_statuses=pending', {
+            method: 'GET',
+            headers: { 'x-api-key': token, 'accept': 'application/json' }
+        });
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar' });
+    }
+}
